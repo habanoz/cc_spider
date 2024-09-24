504b71a373537fb0bde6a26c7af0f89301eb63be
habanoz <huseyinabanox@gmail.com>
2024-09-24 22:16:43 +0300

    add stop words

--- datatrove/process_common_crawl_fetched_files.py
+++ datatrove/process_common_crawl_fetched_files.py
@@ -13,6 +13,8 @@ from datatrove.pipeline.writers.jsonl import JsonlWriter
 from datatrove.utils.typeshelper import Languages
 from datatrove.pipeline.formatters import QuoteFixer
 
+stop_words=["ve","bir","da","de","bu","için","ile","olarak","hakkında","çok","ise","ama","gibi","ne","var","bu","şu","o","biz","siz","onlar","ben","sen","benim","ya","göre","kadar","sonra","her","diye","yüzde","ilgili" ]
+
 SUBDIR="aa"
 MAIN_OUTPUT_PATH = f"/media/melek/depo/work/{SUBDIR}/processed/extract_text"
 TOTAL_TASKS = 4
@@ -23,7 +25,7 @@ executor = LocalPipelineExecutor(
         Trafilatura(favour_precision=True,timeout=0.99),
         QuoteFixer(),
         GopherRepetitionFilter(exclusion_writer=JsonlWriter(f"{MAIN_OUTPUT_PATH}/removed/repetitive"),language=Languages.turkish),
-        GopherQualityFilter(exclusion_writer=JsonlWriter(f"{MAIN_OUTPUT_PATH}/removed/quality"), language=Languages.turkish, max_non_alpha_words_ratio=0.7),
+        GopherQualityFilter(exclusion_writer=JsonlWriter(f"{MAIN_OUTPUT_PATH}/removed/quality"), language=Languages.turkish, max_non_alpha_words_ratio=0.7,stop_words=stop_words),
         FineWebQualityFilter(language=Languages.turkish),
         JsonlWriter(f"{MAIN_OUTPUT_PATH}/text"),
     ],
